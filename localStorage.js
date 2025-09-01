e0747341e4be123f5d5a996758da23dfb1e93ae1
AMMAR RAJABI <131190993+fante123456@users.noreply.github.com>
2025-09-01 16:45:05 +0200

    Create localStorage.js

---
+++ localStorage.js
@@ -0,0 +1,32 @@
+// src/storage/localStorage.js
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// Save any data to storage
+export const saveData = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {
+    console.error('Error saving data', e);
+  }
+};
+
+// Get data from storage
+export const getData = async (key) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.error('Error reading data', e);
+    return null;
+  }
+};
+
+// Remove data
+export const removeData = async (key) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (e) {
+    console.error('Error removing data', e);
+  }
+};
